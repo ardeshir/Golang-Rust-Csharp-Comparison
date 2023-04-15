12f68c2dd130a437d780717d0b529ac9b519ddb0
ardeshir sepahsalar <ardeshir.org@gmail.com>
2023-04-15 13:19:24 -0500

    adding cli commandline

--- csharp/csv_parser/Main.cs
+++ csharp/csv_parser/Main.cs
@@ -10,12 +10,19 @@ namespace CsvParser
     {  
         static void Main(string[] args)  
         {  
+            // Check if the user has provided a CSV file URL  
+            string csvFileUrl = "https://www.ardeshir.io/file.csv"; // Default URL  
+            if (args.Length > 0)  
+            {  
+                csvFileUrl = args[0];  
+            }  
+              
             List<string[]> data = new List<string[]>();  
   
             // Fetch the CSV file from the internet  
             using (var httpClient = new HttpClient())  
             {  
-                using (var stream = httpClient.GetStreamAsync("https://www.ardeshir.io/file.csv").Result)  
+                using (var stream = httpClient.GetStreamAsync(csvFileUrl).Result)  
                 {  
                     using (var reader = new StreamReader(stream))  
                     {  
@@ -38,21 +45,23 @@ namespace CsvParser
                 {  
                     try  
                     {  
-                        var createTableCommand = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS api (id SERIAL PRIMARY KEY, url TEXT, name TEXT)", connection);  
+                        var createTableCommand = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS api (id SERIAL PRIMARY KEY, url TEXT, name TEXT, created INTEGER)", connection);  
                         createTableCommand.ExecuteNonQuery();  
   
-                        var insertCommand = new NpgsqlCommand("INSERT INTO api (id, url, name) VALUES (@id, @url, @name)", connection);  
+                        var insertCommand = new NpgsqlCommand("INSERT INTO api (id, url, name, created) VALUES (@id, @url, @name, @created)", connection);  
                         insertCommand.Parameters.AddWithValue("@id", 0);  
                         insertCommand.Parameters.AddWithValue("@url", 1);  
-                        insertCommand.Parameters.AddWithValue("@name", 2);  
+                        insertCommand.Parameters.AddWithValue("@name", 2); 
+                        insertCommand.Parameters.AddWithValue("@created",3); 
   
                         for (int i = 1; i < data.Count; i++)  
                         {  
                             insertCommand.Parameters[0].Value = Convert.ToInt32(data[i][0]);  
                             insertCommand.Parameters[1].Value = data[i][1];  
-                            insertCommand.Parameters[2].Value = data[i][2];  
-                            //insertCommand.Parameters[3].Value = data[i][3]; 
-                            //insertCommand.Parameters[4].Value = data[i][4]; 
+                            insertCommand.Parameters[2].Value = data[i][2]; 
+                            insertCommand.Parameters[3].Value = Convert.ToInt32(data[i][3]);   
+                            // insertCommand.Parameters[3].Value = data[i][3];   
+                            //insertCommand.Parameters[4].Value = data[i][4];   
                             insertCommand.ExecuteNonQuery();  
                         }  
   
