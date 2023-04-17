ec7bc6bffcaaae5f7b748fd57e455e4e47ecde07
ardeshir sepahsalar <ardeshir.org@gmail.com>
2023-04-16 20:01:30 -0500

    clean up

--- csharp/csv_parser/Main.cs
+++ csharp/csv_parser/Main.cs
@@ -36,12 +36,6 @@ namespace CsvParser  {
                     try   {  
                         var createTableCommand = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS api (id SERIAL PRIMARY KEY, url TEXT, name TEXT, created INTEGER)", connection);  
                         createTableCommand.ExecuteNonQuery();  
-  
-                        // var insertCommand = new NpgsqlCommand("INSERT INTO api (url, name, created) VALUES (@url, @name, @created)", connection);  
-                        // insertCommand.Parameters.AddWithValue("@id", 0);  
-                        //insertCommand.Parameters.AddWithValue("@url", 1);  
-                        //insertCommand.Parameters.AddWithValue("@name", 2); 
-                        //insertCommand.Parameters.AddWithValue("@created",3); 
 
                         var insertCommand = new NpgsqlCommand("INSERT INTO api (url, name, created) VALUES (@url, @name, @created)", connection);    
                         insertCommand.Parameters.AddWithValue("@url", "");    
@@ -51,9 +45,7 @@ namespace CsvParser  {
                         for (int i = 1; i < data.Count; i++)  {  
                             insertCommand.Parameters[0].Value = data[i][1];    
                             insertCommand.Parameters[1].Value = data[i][2];   
-                            insertCommand.Parameters[2].Value = Convert.ToInt32(data[i][3]);   
-                            // insertCommand.Parameters[3].Value = data[i][3];   
-                            //insertCommand.Parameters[4].Value = data[i][4];   
+                            insertCommand.Parameters[2].Value = Convert.ToInt32(data[i][3]);     
                             insertCommand.ExecuteNonQuery();  
                         }  
   
